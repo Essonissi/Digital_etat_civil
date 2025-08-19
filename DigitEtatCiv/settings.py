96159337357d78b1a3da7867cb6f419762d01f35
AMESONSTACK <nathanaelamedro@gmail.com>
2025-08-19 11:03:47 +0000

    arrangement de allowed localhost

--- DigitEtatCiv/settings.py
+++ DigitEtatCiv/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-j_!9chmgl7tcrx!=24n73
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='digital-etat-civil.onrender.com,localhost,127.0.0.1').split(',')
 
 # Application definition
 INSTALLED_APPS = [
